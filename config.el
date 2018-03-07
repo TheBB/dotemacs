26b4fdb22a5c4c16cc626bf401de47fcfda03c7c
Eivind Fonn <evfonn@gmail.com>
2018-03-07 17:31:17 +0100

    Clean up config.el

--- config.el
+++ config.el
@@ -1,19 +1,19 @@
 (setq package-user-dir (concat bb-cfg-dir "elpa/")
       package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-			 ("melpa" . "https://melpa.org/packages/"))
+                         ("melpa" . "https://melpa.org/packages/"))
 
       bb-packages
       `(
         general
-	monokai-theme
+        monokai-theme
         (spaceline :location ,(concat bb-cfg-dir "third-party/spaceline"))
 
-	(emacs :location internal)
-	(evil :location ,(concat bb-cfg-dir "third-party/evil"))
+        (emacs :location internal)
+        (evil :location ,(concat bb-cfg-dir "third-party/evil"))
         helm
         page-break-lines
 
+        ;; Packages needed as dependencies
         dash
         s
-        powerline
-	))
+        powerline))
