aca1dc2bc4968dc186444ff32cfc87f0df93afdd
Eivind Fonn <evfonn@gmail.com>
2018-03-07 17:51:35 +0100

    Make right act as return in helm

--- config/bb-helm.el
+++ config/bb-helm.el
@@ -5,6 +5,7 @@
         helm-display-buffer-width 80)
   (add-hook 'helm-minibuffer-set-up-hook 'helm-hide-minibuffer-maybe)
   (with-eval-after-load 'helm-files
+    (define-key helm-find-files-map (kbd "<right>") 'helm-ff-RET)
     (advice-add 'helm-ff-filter-candidate-one-by-one
                 :around 'bb-helm-ff-filter-candidate-one-by-one)
     (advice-add 'helm-find-files-up-one-level
