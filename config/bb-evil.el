49cd1b67f84a189adc1f99c87e08a90cfec02c40
Eivind Fonn <evfonn@gmail.com>
2018-03-07 18:58:06 +0100

    Evil cursors from Spacemacs

--- config/bb-evil.el
+++ config/bb-evil.el
@@ -1,4 +1,11 @@
 (bb-package evil
   :init
+  (setq-default
+   evil-normal-state-cursor '("DarkGoldenrod2" box)
+   evil-insert-state-cursor '("chartreuse3" (bar . 2))
+   evil-emacs-state-cursor '("SkyBlue2" box)
+   evil-replace-state-cursor '("chocolate" (hbar . 2))
+   evil-visual-state-cursor '("gray" (hbar . 2))
+   evil-motion-state-cursor '("plum3" box))
   (require 'evil)
   (evil-mode))
