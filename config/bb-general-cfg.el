4ab30d349303625ead2ae7ed2ab4e999d13d8dd3
Eivind Fonn <evfonn@gmail.com>
2018-03-08 13:29:04 +0100

    Dispatch system for major-mode leader keys

--- config/bb-general-cfg.el
+++ config/bb-general-cfg.el
@@ -2,3 +2,30 @@
   `(progn
      (require 'general)
      (general-define-key :prefix "SPC" :states '(normal motion) ,@args)))
+
+(defmacro bb-create-dispatch (keys)
+  (let ((funcname (intern (format "bb-dispatch-%s" keys)))
+        (varname (intern (format "bb-dispatch-table-%s" keys))))
+    `(progn
+       (unless (boundp ',varname)
+         (defvar ,varname nil)
+         (defun ,funcname ()
+           (interactive)
+           (catch 'done
+             (dolist (entry ,varname)
+               (when (derived-mode-p (car entry))
+                 (throw 'done (call-interactively (cdr entry)))))
+             (user-error "No dispatch found for \"%s\" in %s" ,keys major-mode)))
+         (bb-leader ,keys ',funcname)))))
+
+(defmacro bb-mm-leader (mode &rest args)
+  (let (bindings)
+    (while args
+      (push (cons (car args) (cadr args)) bindings)
+      (setq args (cddr args)))
+    `(progn
+       ,@(cl-loop for binding in bindings
+                  collect `(bb-create-dispatch ,(car binding)))
+       ,@(cl-loop for binding in bindings
+                  collect `(push '(,mode . ,(cdr binding))
+                                 ,(intern (format "bb-dispatch-table-%s" (car binding))))))))
