1330d173e25ea92e6216fc09b1995ee199760f1e
Eivind Fonn <evfonn@gmail.com>
2018-03-12 19:25:30 +0100

    Flycheck error list looks ugly

--- config/bb-flycheck.el
+++ config/bb-flycheck.el
@@ -1,3 +1,8 @@
 (bb-package flycheck
+
+  :init
+  (with-eval-after-load 'flycheck
+    (aset flycheck-error-list-format 4 '("Message" 0 t)))
+
   :post-init lsp-mode
   (add-hook 'lsp-mode 'flycheck-mode))
