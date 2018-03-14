50f8a433288ceb2a0b6105f7643dae721561dbc0
Eivind Fonn <evfonn@gmail.com>
2018-03-14 18:37:11 +0100

    Binding for projectile-find-other-file

--- config/bb-projectile.el
+++ config/bb-projectile.el
@@ -2,5 +2,6 @@
   :init
   (setq projectile-known-projects-file
         (bb-dir ".cache/projectile-bookmarks.eld"))
+  (bb-leader "ga" 'projectile-find-other-file)
   (projectile-mode)
   (diminish 'projectile-mode))
