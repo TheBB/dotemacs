7af3b34dd98c6679f64e2e6d604b57c8eafbb648
Eivind Fonn <evfonn@gmail.com>
2018-03-18 09:34:43 +0100

    Turn off smartparens highlighting

--- config/bb-smartparens.el
+++ config/bb-smartparens.el
@@ -1,5 +1,8 @@
 (bb-package smartparens
   :init
+  (setq sp-highlight-pair-overlay nil
+        sp-highlight-wrap-overlay nil
+        sp-highlight-wrap-tag-overlay nil)
   (add-hook 'prog-mode-hook 'smartparens-mode)
   (with-eval-after-load 'smartparens
     (require 'smartparens-config)
