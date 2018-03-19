e189b78e45ea06eef648baaf3ada2584d56e7765
Eivind Fonn <evfonn@gmail.com>
2018-03-19 10:48:37 +0100

    Don't highlight symbol at point, please, lsp

--- config/bb-lsp-mode.el
+++ config/bb-lsp-mode.el
@@ -1,7 +1,7 @@
 (bb-package lsp-mode
   :init
   (bb-leader "tl" 'lsp-mode)
-  (require 'lsp-mode)
   (put 'lsp-define-stdio-client 'lisp-indent-function 2)
-  (with-eval-after-load 'lsp-mode
-    (diminish 'lsp-mode "l")))
+  (setq lsp-highlight-symbol-at-point nil)
+  (require 'lsp-mode)
+  (diminish 'lsp-mode "l"))
