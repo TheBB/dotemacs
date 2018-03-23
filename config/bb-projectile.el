c80597f3d7e8098b3e0ab307877b42f2ad5589ae
Eivind Fonn <evfonn@gmail.com>
2018-03-23 17:05:15 +0100

    Move projectile cache out of the way

--- config/bb-projectile.el
+++ config/bb-projectile.el
@@ -1,7 +1,7 @@
 (bb-package projectile
   :init
-  (setq projectile-known-projects-file
-        (bb-dir ".cache/projectile-bookmarks.eld"))
+  (setq projectile-known-projects-file (bb-dir ".cache/projectile-bookmarks.eld")
+        projectile-cache-file (bb-dir ".cache/projectile.cache"))
   (bb-leader "ga" 'projectile-find-other-file)
   (projectile-mode)
   (diminish 'projectile-mode))
