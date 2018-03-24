978a74c893399dd5d71bfc64e8b03ed9ec3559e6
Eivind Fonn <evfonn@gmail.com>
2018-03-24 14:03:02 +0100

    Don't highlight operators in python-mode

--- config/bb-highlight-operators-cfg.el
+++ config/bb-highlight-operators-cfg.el
@@ -1,3 +1,3 @@
 (defun bb-maybe-highlight-operators ()
-  (unless (derived-mode-p 'lisp-mode 'scheme-mode 'emacs-lisp-mode)
+  (unless (derived-mode-p 'lisp-mode 'scheme-mode 'emacs-lisp-mode 'python-mode)
     (highlight-operators-mode)))
