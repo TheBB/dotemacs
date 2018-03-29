d2ed1339f6b35347df16bbc1101d5138bd6fefec
Eivind Fonn <evfonn@gmail.com>
2018-03-29 09:56:44 +0200

    Move modeline to init.el

--- init.el
+++ init.el
@@ -46,6 +46,46 @@
 
 
 
+;; Modeline
+
+(use-package spaceline-segments
+  :init
+  (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state
+	spaceline-minor-modes-separator "")
+  :config
+  (spaceline-generate
+    (((persp-name workspace-number window-number) :fallback evil-state :face highlight-face :priority 100)
+     (anzu :priority 95)
+     (auto-compile)
+     ((buffer-modified buffer-size buffer-id remote-host) :priority 98)
+     (major-mode :priority 79)
+     (process :when active)
+     ((flycheck-error flycheck-warning flycheck-info) :when active :priority 89)
+     (minor-modes :when active :priority 9)
+     (erc-track :when active)
+     (version-control :when active :priority 78))
+    (which-function
+     (python-pyvenv :fallback python-pyenv)
+     (purpose :priority 94)
+     (selection-info :priority 95)
+     input-method
+     ((buffer-encoding-abbrev
+       point-position
+       line-column)
+      :separator " | "
+      :priority 96)
+     (global :when active)
+     (buffer-position :priority 99)
+     (hud :priority 99)))
+  (setq-default mode-line-format '("%e" (:eval (spaceline-ml-main)))))
+
+(use-package spaceline-config
+  :after helm
+  :config
+  (spaceline-helm-mode))
+
+
+
 ;; General Emacs settings (built-ins, etc.)
 
 (setq user-init-file (or load-file-name buffer-file-name)
