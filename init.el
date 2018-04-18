882fdb993ad0e128173feb3612d531d5f14327b5
Eivind Fonn <evfonn@gmail.com>
2018-04-18 11:53:03 +0200

    Uniform font sizes please, monokai

--- init.el
+++ init.el
@@ -87,6 +87,12 @@
 
 ;; Theme
 
+(setq monokai-height-minus-1 1.0
+      monokai-height-plus-1 1.0
+      monokai-height-plus-2 1.0
+      monokai-height-plus-3 1.0
+      monokai-height-plus-4 1.0)
+
 (load-theme 'monokai 'noconfirm)
 (require 'color)
 
