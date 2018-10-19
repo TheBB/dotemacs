0c0985c049de59e9407c4521bd628562345ea7d1
Eivind Fonn <evfonn@gmail.com>
2018-10-19 19:26:04 +0200

    Add some safe local variable values

--- custom.el
+++ custom.el
@@ -3,7 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values '((helm-make-build-dir . "Apps/bld-sd"))))
+ '(safe-local-variable-values
+   '((projectile-project-run-cmd . "mkdir -p build; cd build; cmake ..; make run")
+     (projectile-project-compilation-cmd . "mkdir -p build; cd build; cmake ..; make")
+     (helm-make-build-dir . "Apps/bld-sd"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
