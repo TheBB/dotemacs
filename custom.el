4c1f4ca201d694a6e48d0a49730966a9ecb9d088
Eivind Fonn <evfonn@gmail.com>
2019-11-06 09:44:53 +0100

    Add more safe local variable values

--- custom.el
+++ custom.el
@@ -4,7 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((projectile-project-run-cmd . "emacs")
+   '((projectile-project-test-cmd . "julia --project=. --eval 'using Pkg; Pkg.test(\"Aroma\")'")
+     (projectile-project-test-cmd . "julia --project=. --color=yes --eval 'using Pkg; Pkg.test(\"Aroma\")'")
+     (projectile-project-run-cmd . "julia --project=. run.jl")
+     (projectile-project-run-cmd . "emacs")
      (projectile-project-compilaton-cmd . "make")
      (helm-make-build-dir . "doc")
      (projectile-project-run-cmd . "mkdir -p build; cd build; cmake ..; make run")
