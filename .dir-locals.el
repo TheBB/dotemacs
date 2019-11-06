abe2f7aa195181a050fa93145d75bfc48614d244
Eivind Fonn <evfonn@gmail.com>
2019-11-06 10:01:39 +0100

    Fix wrong variable name

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((projectile-project-compilaton-cmd . "make")
+((nil . ((projectile-project-compilation-cmd . "make build")
          (projectile-project-run-cmd . "emacs"))))
--- custom.el
+++ custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((projectile-project-test-cmd . "julia --project=. --eval 'using Pkg; Pkg.test(\"Aroma\")'")
+   '((projectile-project-compilation-cmd . "make build")
+     (projectile-project-test-cmd . "julia --project=. --eval 'using Pkg; Pkg.test(\"Aroma\")'")
      (projectile-project-test-cmd . "julia --project=. --color=yes --eval 'using Pkg; Pkg.test(\"Aroma\")'")
      (projectile-project-run-cmd . "julia --project=. run.jl")
      (projectile-project-run-cmd . "emacs")
