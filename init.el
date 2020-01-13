fa0bfbcab662f8f58cec9825fde63dff87890d7a
Eivind Fonn <evfonn@gmail.com>
2020-01-13 13:31:34 +0100

    Smartly choose undo-fu as undo provider

--- init.el
+++ init.el
@@ -355,11 +355,15 @@
         evil-replace-state-cursor '("chocolate" (hbar . 2))
         evil-visual-state-cursor '("gray" (hbar . 2))
         evil-motion-state-cursor '("plum3" box)
-        evil-undo-provider 'undo-tree
         evil-want-integration t
         evil-want-keybinding nil
         evil-want-C-u-scroll t)
   :config
+
+  ;; Use undo-fu as undo provider if we have the right evil branch
+  (when (boundp 'evil-undo-provider)
+    (setq evil-undo-list-pointer 'undo-fu))
+
   (evil-mode)
   (bb-add-hook evil-insert-state-exit-hook
     (deactivate-mark))
@@ -1039,7 +1043,7 @@
   :hook (org-mode . typo-mode))
 
 (use-package undo-tree
-  :if (eq 'undo-tree evil-undo-provider)
+  :if (eq 'undo-tree (bound-and-true-p evil-undo-provider))
   :diminish undo-tree-mode
   :init
   (setq undo-tree-enable-undo-in-region nil)
